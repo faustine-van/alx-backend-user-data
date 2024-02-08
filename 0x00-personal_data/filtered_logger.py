89207d22d1ed9612db71c8658a1d6401e7a7511d
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-08 15:03:04 +0200

    create connection from database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -66,10 +66,14 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """connector to database"""
-    db = mysql.connector.connect(
-        host=os.getenv('PERSONAL_DATA_DB_HOST'),
-        user=os.getenv('PERSONAL_DATA_DB_USERNAME'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD'),
-        database=os.getenv('PERSONAL_DATA_DB_NAME')
-    )
-    return db
+    try:
+        db = mysql.connector.connect(
+            host=os.getenv('PERSONAL_DATA_DB_HOST'),
+            user=os.getenv('PERSONAL_DATA_DB_USERNAME'),
+            password=os.getenv('PERSONAL_DATA_DB_PASSWORD'),
+            database=os.getenv('PERSONAL_DATA_DB_NAME')
+        )
+        return db
+    except mysql.connector.Error as error:
+        print(error)
+        return None
