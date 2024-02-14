644aeaa8b70635d56a4ae4525c13541bcb911f62
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-14 22:43:55 +0200

    manage expiration date to a Session ID.

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -13,7 +13,12 @@ class SessionExpAuth(SessionAuth):
     """
     def __init__(self):
         """initialize variables"""
-        self.session_duration = int(os.environ.get('SESSION_DURATION'))
+         # Get the value of the environment variable SESSION_DURATION
+        session_duration_str = os.environ.get('SESSION_DURATION')
+        try:
+            self.session_duration = int(session_duration_str)
+        except (ValueError, TypeError):
+            self.session_duration = 0
 
     def create_session(self, user_id=None):
         """create session_id with expiration data"""
@@ -32,15 +37,20 @@ class SessionExpAuth(SessionAuth):
         if session_id is None \
                 or session_id not in self.user_id_by_session_id.keys():
             return None
+
         session_dict = self.user_id_by_session_id.get(session_id)
-        if 'created_at' not in session_dict.keys():
+        if session_dict is None:
             return None
-        user_id = self.user_id_by_session_id.get(session_id).get('user_id')
-        if self.session_duration == 0 or self.session_duration < 0:
-            return user_id
-        created_at = self.user_id_by_session_id.get(session_id
-                                                    ).get('created_at')
-        date_after_sec = created_at + timedelta(seconds=self.session_duration)
-        if date_after_sec < datetime.now():
+
+        created_at = session_dict.get('created_at')
+        if created_at is None:
             return None
-        return user_id
+
+        if self.session_duration <= 0:
+            return session_dict.get('user_id')
+
+        current_time = datetime.now()
+        expiration_time = created_at + timedelta(seconds=self.session_duration)
+        if expiration_time < current_time:
+            return None
+        return session_dict.get('user_id')
