84609aa651788a1994eb04db883873fb21235116
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-16 08:56:46 +0200

    test

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -14,7 +14,7 @@ class SessionDBAuth(SessionExpAuth):
         """creates and stores new instance of UserSession
             and returns the Session ID
         """
-        if user_id is None:
+        if user_id is None or not isinstance(user_id, str):
             return None
 
         session_id = super().create_session(user_id)
