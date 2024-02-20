054136fc29fb6c0ba877d0215f012cc4c4169b86
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-20 21:26:32 +0200

    user model

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
         """add user to the database"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
