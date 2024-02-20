18ffe7ce483f16a18c1c3c4b58670e9dc4d26267
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-21 00:05:23 +0200

    generate new unique id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,17 +64,14 @@ class Auth:
 
     def valid_login(self, email: str, password: str) -> bool:
         """validate login"""
-        try:
-            user = self._db.find_user_by(email=email)
-            if user:
-                bytePass = password.encode('utf-8')
-                if bcrypt.checkpw(bytePass, user.hashed_password):
-                    return True
-                else:
-                    return False
-            return False
-        except NoResultFound:
-            pass
+        user = self._db._session.query(User).filter_by(email=email).first()
+        if user:
+            bytePass = password.encode('utf-8')
+            if bcrypt.checkpw(bytePass, user.hashed_password):
+                return True
+            else:
+                return False
+        return False
 
     def create_session(self, email: str) -> str:
         """return session id for user corresponding to the emai
