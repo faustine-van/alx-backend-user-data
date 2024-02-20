091b849198cddb34d2a0adf6b708cac3e6b8fbd4
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-20 22:37:41 +0200

    user model

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """DB module
 """
-from typing import TypeVar
+from typing import TypeVar, Dict
 from sqlalchemy import create_engine
 from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from sqlalchemy.orm import sessionmaker
@@ -33,15 +33,12 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """add user to the database"""
-        try:
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs) -> TypeVar('User'):
+    def find_user_by(self, **kwargs: Dict) -> User:
         """returns the first row found in the users table as
             filtered by the method’s input arguments.
         """
