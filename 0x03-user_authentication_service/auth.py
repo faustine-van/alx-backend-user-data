d87a98320d014c7621cd3455b9798314ad068f00
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-21 13:44:56 +0200

    register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -135,11 +135,10 @@ class Auth:
         Returns:
             str: Return the token if user exists or raise error if not.
         """
-        user = self._db._session.query(User
-                                       ).filter_by(reset_token=reset_token
-                                                   ).first()
-        if not user:
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_pass = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_pass,
+                                 reset_token=None)
+        except NoResultFound:
             raise ValueError()
-        hashed_pass = _hash_password(password)
-        self._db.update_user(user.id, hashed_password=hashed_pass,
-                             reset_token=None)
